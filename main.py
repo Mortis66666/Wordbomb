779ee4aa5dcdb33e666f2b9ba6705791f61ffe40
Mortis66666 <mortisloverbs@gmail.com>
2022-02-13 11:47:48 +0800

    Added bombs

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pygame
+import random
+import time
+from threading import Thread
+
 
 
 pygame.font.init()
@@ -6,6 +10,8 @@ pygame.font.init()
 width, height = 1000, 600
 win = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Word Bomb")
+
+font = pygame.font.SysFont("archivo", 30)
 
 
 #colours
@@ -18,23 +24,45 @@ class Bomb:
 
     def __init__(self, index) -> None:
         self.index = index
-        self.y = 0
-        self.x = index * 100
+        self.y = 30
+        self.x = index * 100 + 30
+        self.available = True
+        self.char = random.choice("`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./").upper()
 
     def blit(self):
-        pygame.draw.circle(win, black, (self.x, self.y), 20)
+        pygame.draw.circle(win, black, (self.x, self.y), 30)
+        text = font.render(self.char, True, white)
+        win.blit(text, (self.x - 10, self.y - 10))
 
 
 
 class Game:
 
+    bombs = []
+
     def draw_window(self):
         win.fill(white)
 
-        Bomb(1).blit()
-
+        for bomb in self.bombs:
+            bomb.blit()
 
         pygame.display.update()
+
+    def random_bombs(self) -> set:
+
+        return set(
+            random.choices(
+                list(range(10)),
+                k = random.randint(1,3)
+            )
+        )
+
+    def make_bombs(self):
+
+        while True:
+            for bomb in self.random_bombs():
+                self.bombs.append(Bomb(bomb))
+            time.sleep(3)
 
     def start(self):
 
@@ -42,22 +70,24 @@ class Game:
         clock = pygame.time.Clock()
         fps = 60
 
+        thread = Thread(target=self.make_bombs)
+        thread.start()
+
 
         while run:
-
             clock.tick(fps)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    run = False
                     pygame.quit()
+                    exit()
+                
+
             self.draw_window()
+
 
 
 if __name__ == '__main__':
     game = Game()
-
     game.start()
-
-        
-
-
+   
